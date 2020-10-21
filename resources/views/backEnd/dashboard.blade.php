74cb53d192b967b8fc8758f38c76418dbb2a5d2e
Le Duc Giac Hoang <leducgiachoang13@gmail.com>
2020-10-21 21:19:21 +0700

    add file

---
+++ resources/views/backEnd/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('template.backend')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @section('container')
+    <h1>Đây là trang dashboard</h1>
+    @endsection
+   
+</body>
+</html>
